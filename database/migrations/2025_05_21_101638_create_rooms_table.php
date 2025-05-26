704ed8210346050c43cf62d7a7e6867c7a0dabde
Sazabe <sergiobraa@gmail.com>
2025-05-26 14:36:15 +0200

    Queda modificar el seeder, no se porque da error

--- database/migrations/2025_05_21_101638_create_rooms_table.php
+++ database/migrations/2025_05_21_101638_create_rooms_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
-            $table->name();
-            $table->foreignId('building_id')->constrained('building')->onDelete('cascade');;
-            $table->enum('type', ['empty', 'reserved', 'occupied','unavailable'])->default('free');
+            $table->foreignId('building_id')->constrained('buildings')->onDelete('cascade');
+            $table->string('name');
+            $table->enum('type', ['empty', 'reserved', 'occupied','unavailable'])->default('empty');
             $table->timestamps();
         });
     }
